01cca568eb93be5d3e3e2a7ca3d9762b654ad0b5
Hamza <hamza.amjad@revolvinggames.com>
2023-08-03 18:20:01 +0500

    2nd Commit

--- Program.cs
+++ Program.cs
@@ -2,8 +2,11 @@
 
 using System.Runtime.InteropServices;
 using System.Collections;
-namespace Training;
+using System.Drawing;
 
+//Alias support in C# 12
+//global using BandPass = (int Min, int Max);
+namespace Training;
 /*Console.WriteLine("Hello, World!");
 int [] source= {1,2,3,4,5};
 var queryInt= from item in source where item <=4 select item;
@@ -28,7 +31,16 @@ public class Person
     public required string LastName { get; set; }
     public required string FirstName { get; set; }
 }
+public interface IAnimal
+{
+    void Speak(); // Traditional interface method
 
+    // Default interface method
+    void Eat()
+    {
+        Console.WriteLine("The animal eats.");
+    }
+}
 public class Everything
 {
     public enum MyEnum
@@ -93,6 +105,49 @@ public class Everything
 
         var p3 = p1 with { X = 1, Y = 4 };
         Console.WriteLine(p3); // output: (1, 4)
+        
+        
+        //Tuples
+        (double, int) t1 = (4.5, 2);
+        Tuples t2 = new Tuples();
+        Console.WriteLine($"Length of Default: {t2.NoOfCharactersInName()}");
+        t2.SetName("Hamza");
+        Console.WriteLine($"Length of my Name is : {t2.NoOfCharactersInName()}");
+
+        //Tuples t3 = new Tuples(name:"Hamza", 2, value:999);
+        
+        //Named arguments can be in any order.
+        Tuples t3 = new Tuples(id:2,name:"Hamza", value:999);
+        (int?, string) t4 = t3.GetATuple();
+        Console.WriteLine($"Tuple Id: {t4.Item1} \n Tuple Name:{t4.Item2}");
+        t4.Item1 = 2123;
+        Console.WriteLine($"Tuple Id: {t4.Item1} \n Tuple Name:{t4.Item2}");
+
+            
+        (int? id, string name) t5 = t3.GetATuple();
+        Console.WriteLine($"Tuple Id: {t5.id} \n Tuple Name:{t5.name}");
+        var (id, name) = t3.GetATuple();
+        Console.WriteLine($"Tuple Id: {id} \n Tuple Name:{name}");
+        
+        var tt = (Sum: 4.5, Count: 3);
+        Console.WriteLine($"Sum of {tt.Count} elements is {tt.Sum}.");
+
+        (double Sum, int Count) d = (4.5, 3);
+        Console.WriteLine($"Sum of {d.Count} elements is {d.Sum}.");
+        //This works only with
+        var (id5,_)= t3.GetATuple();
+        (int? , string _) t7 = t3.GetATuple();
+        Console.WriteLine(t3.val());
+        //(string,int?, int?) t8 = t3;
+        Math.DisplayNothing();
+        IMinusTwo<int> m = new Math();
+        Console.WriteLine("----------------------------------------------------------------");
+        m.DisplayHello();
+        
+        //Error on Dog.Eat();
+        /*Dog dog = new Dog();
+        dog.Speak();  // Output: Woof!
+        dog.Eat();    // Output: The animal eats.*/
     }
 }
 
@@ -149,4 +204,137 @@ public struct TempStruct
     {
         Name = "Hamza";
     }
+}
+
+public class Tuples
+{
+    private string _tupleName = null;
+    private int? _tupleId = null;
+    private int? _tupleValue;
+
+    public Tuples()
+    {
+        //_tupleName = "Default Name";
+        _tupleId = null;
+    }
+    
+    public Tuples(string name, int id, int? value=null)
+    {
+        _tupleName = name;
+        _tupleId = id;
+        _tupleValue = value;
+        
+    }
+
+    public int NoOfCharactersInName()
+    {
+        if (_tupleName == null)
+            return 0;
+        //return _tupleName.Length;
+        
+        //Checking foreach
+        int countChar = 0;
+        foreach (var character in _tupleName)
+        {
+            countChar++;
+        }
+
+        //int a = _tupleId ?? 0;
+        return countChar;
+    }
+    public void SetName(string name)
+    {
+        this._tupleName = name;
+    }
+
+    public (int?, string) GetATuple()
+    {
+        return (_tupleId, _tupleName);
+    }
+
+    public double val()
+    {
+        double n = 1 / 2.0;
+        return n;
+    }
+}
+
+interface IAddTwo
+{
+    int Add(int val);
+}
+interface IEquatable
+{
+    bool Equals(int obj);
+}
+
+public interface IMinusTwo<T>
+{
+    int Minus(T val);
+
+    void DisplayHello()
+    {
+        Console.WriteLine("Hello!!!!");
+    }
+}
+
+public class Math: IAddTwo, IMinusTwo<int>
+{
+    public int Add(int val)
+    {
+        //throw new NotImplementedException();
+        return 2 + val;
+    }
+
+    public int Minus(int val)
+    {
+        //throw new NotImplementedException();
+        return val - 2;
+        
+    }
+
+    public static void DisplayNothing()
+    {
+        Console.WriteLine("Nothing!!");
+    }
+}
+
+public class Dog : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+public abstract class Motorcycle
+{
+    // Anyone can call this.
+    public void StartEngine() {/* Method statements here */ }
+
+    // Only derived classes can call this.
+    protected void AddGas(int gallons) { /* Method statements here */ }
+
+    // Derived classes can override the base class implementation.
+    public virtual int Drive(int miles, int speed) { /* Method statements here */ return 1; }
+
+    // Derived classes can override the base class implementation.
+    public virtual int Drive(TimeSpan time, int speed) { /* Method statements here */ return 0; }
+
+    // Derived classes must implement this.
+    public abstract double GetTopSpeed();
+}
+
+public class Cycle: Motorcycle
+{
+    public override double GetTopSpeed()
+    {
+        return 60.0;
+    }
+    
+    //There is no suitable method for override
+    /*public override void AddGas()
+    {
+        
+    }*/
 }
