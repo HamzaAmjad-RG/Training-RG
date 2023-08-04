0be9968c2de8c78e297b5627a5cbca9d047550a1
Hamza <hamza.amjad@revolvinggames.com>
2023-08-04 18:45:58 +0500

    3rd Commit

--- Program.cs
+++ Program.cs
@@ -2,7 +2,10 @@
 
 using System.Runtime.InteropServices;
 using System.Collections;
+using System.Data.SqlTypes;
 using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 
 //Alias support in C# 12
 //global using BandPass = (int Min, int Max);
@@ -26,6 +29,25 @@ Console.WriteLine($"Type of QueryString(Type Var): {queryString.GetType()}");
 Console.Read(); //Read int
 Console.ReadLine();// Read String
 */
+public delegate void MyDelegate(int num);
+
+public delegate int MyDelegate2(int num);
+
+public class LearningDelegates
+{
+    public static void MyMethod(int a)
+    {
+        a++;
+        Console.WriteLine($"Incremented Value is: {a}");
+    }
+
+    public static void MyMethod2(MyDelegate2 del, int a)
+    {
+        a++;
+        a = del(a);
+        Console.WriteLine($"A now is: {a}");
+    }
+}
 public class Person
 {
     public required string LastName { get; set; }
@@ -140,7 +162,9 @@ public class Everything
         Console.WriteLine(t3.val());
         //(string,int?, int?) t8 = t3;
         Math.DisplayNothing();
-        IMinusTwo<int> m = new Math();
+        IMinusTwo<int> m = new Math(45);
+        Math m1 = new Math(50);
+        m1.IsEven();
         Console.WriteLine("----------------------------------------------------------------");
         m.DisplayHello();
         
@@ -148,9 +172,145 @@ public class Everything
         /*Dog dog = new Dog();
         dog.Speak();  // Output: Woof!
         dog.Eat();    // Output: The animal eats.*/
+
+        MyDelegate del1 = new MyDelegate(LearningDelegates.MyMethod);
+        del1.Invoke(3);
+        del1(2);
+
+        //Anonymous Methods
+        MyDelegate del2= delegate(int num) { num--; Console.WriteLine($"Decremented Value: {num}");  };
+        del2.Invoke(2);
+        del2(2);
+
+
+        LearningDelegates.MyMethod2(delegate(int num) { num--;
+            return num;
+        },12 );
+
+        //Lambda Expressions    //Simplified Anonymous Function
+
+
+        //Lambda Statement
+        MyDelegate del3 = (num) =>
+        {
+            num--;
+            Console.WriteLine($"Decremented Value: {num}");
+        };
+
+        //Lambada Expression
+        MyDelegate del4 = (num) => Console.WriteLine($"Value of num is: {num}");
+        MyDelegate2 del5 = (num) => --num;
+        del4(3);
+        Console.WriteLine(del5(10));
+
+        //You can also use lambda expressions when you write LINQ in C#, as the following example shows:
+        int[] numbers1 = { 2, 3, 4, 5 };
+        var squaredNumbers = numbers1.Select(x3 => x3 * x3);
+        Console.WriteLine(string.Join(" ", squaredNumbers));
+        //Action Delegate for no return type
+        Action del6 = () => Console.WriteLine("Heyyyyyyyyyyyyyyyyy");
+        Action<int> del7 = (n1) => Console.WriteLine($"Heyyy n1 is :{n1}");
+
+        //Func delegate for reutrn type
+        Func<int,int> del8 = (num) => num * num;
+        //del8 = (_) => 33;
+        del6();
+        del7(del8(22));
+
+
+        var firstNumbersLessThanSix = numbers.TakeWhile(n => n < 6);
+        Console.WriteLine(string.Join(" ", firstNumbersLessThanSix));
+        //var parse = s => int.Parse(s); // ERROR: Not enough type info in the lambda
+
+        var l1 = new LearningProperties { firstName = "Hamza" };
+        Console.WriteLine(l1.firstName);
+        Console.WriteLine("First Full Name:" + l1.fullName);
+        l1.DisplayFullName();
+        Console.WriteLine("Second Full Name:" + l1.fullName);
+
+        //Indexers
+        var le = new LearningIndexers();
+        le[2] = 12;
+    }
+    public IEnumerable<int> GetSingleDigitNumbers()
+    {
+        yield return 0;
+        yield return 1;
+        yield return 2;
+        yield return 3;
+        yield return 4;
+        yield return 5;
+        yield return 6;
+        yield return 7;
+        yield return 8;
+        yield return 9;
     }
 }
 
+public class LearningIndexers
+{
+    private int[] _store = new int[3];
+    private int[] _st = new int[3];
+    public int this[int index]
+    {
+        get => _store[index];
+        set => _store[index] = value;
+    }
+    public int this[int index, int i]
+    {
+        get => _st[index];
+        set => _st[index] = value;
+    }
+}
+public class LearningProperties
+{
+
+    public string firstName { get; set; }
+    public string? middleName { get; set; } = string.Empty;
+    public string lastName { get; set; }
+    public string fullName { get =>$"{firstName} {lastName}";
+        set => _fullName = value;
+    }
+
+    private string _fullName;
+    private string _fatherName;
+    private string _motherName;
+    private string _brotherName;
+
+    public string fatherName
+    {
+        get => _fatherName;
+        set => _fatherName = value;
+    }
+
+    public string motherName
+    {
+        get => _motherName;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("First name must not be blank!!!");
+            _motherName = value;
+        }
+    }
+
+
+    public string brotherName {
+        get => _brotherName;
+        set => _brotherName = (!string.IsNullOrWhiteSpace(value))
+            ? value
+            : throw new AggregateException("Having a brother is MUSTTT!!!!");
+    }
+
+    public string? babyName { get; private set; }
+
+    public void DisplayFullName()
+    {
+        _fullName = "Hamza Ali Amjad";
+        Console.WriteLine(_fullName);
+    }
+
+}
 public class Foo
 {
     public int N;
@@ -280,6 +440,12 @@ public interface IMinusTwo<T>
 
 public class Math: IAddTwo, IMinusTwo<int>
 {
+    public int _num { get; }
+
+    public Math(int num)
+    {
+        _num = num;
+    }
     public int Add(int val)
     {
         //throw new NotImplementedException();
@@ -290,12 +456,21 @@ public class Math: IAddTwo, IMinusTwo<int>
     {
         //throw new NotImplementedException();
         return val - 2;
-        
     }
-
     public static void DisplayNothing()
     {
         Console.WriteLine("Nothing!!");
+    }
+
+
+}
+
+
+public static class MathExtension
+{
+    public static bool IsEven(this Math self)
+    {
+        return self._num % 2 == 0;
     }
 }
 
